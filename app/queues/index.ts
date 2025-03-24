7053b25650421e0799811c74f6e6d3b76e72ae7d
evidence-codes <adejuwonevidence181@gmail.com>
2025-03-24 23:07:33 +0100

    chore: comment missing file

--- app/queues/index.ts
+++ app/queues/index.ts
@@ -1,11 +1,6 @@
 // DO NOT REMOVE THE IMPORTS BELOW, THEY ARE NECESSARY FOR THE QUEUES TO WORK; YOU CAN ADD YOUR OWN IMPORTS AS WELL
 
-import * as achievementQueue from './achievementQueue';
-import * as pointDeductionQueue from './pointDeductionQueue';
-import * as reminderQueue from './reminderQueue';
-import * as giftQueue from './giftQueue';
 import * as otpQueue from './otpQueue';
-import * as notificationQueue from './notificationQueue';
-import * as connectionSyncResetQueue from './connectionSyncResetQueue';
-import * as NoSessionScheduleQueue from './NoSessionScheduleQueue';
+// import * as notificationQueue from './notificationQueue';
+
 export default {};
